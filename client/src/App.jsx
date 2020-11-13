4992082f37de9ff534f449b8a76b32935f10721c
atsteinberg <alexts404dev@gmail.com>
2020-11-13 12:18:19 +0100

    fix: html tags in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,23 @@ import React from 'react';
 import styles from './App.module.css';
 
 import Header from './components/Header/Header';
+import Center from './components/Center/Center';
+import InputForm from './components/InputForm/InputForm';
 
 function App() {
   return (
-    <body className={styles.App}>
+    <div className={styles.App}>
       <header>
         <Header title="Destroy Da One Ring" />
       </header>
-      <section>{/* <InputForm />
-        <ResultDisplay /> */}</section>
-    </body>
+      <section>
+        {/* <InputForm />
+        <ResultDisplay /> */}
+        <Center>
+          <InputForm />
+        </Center>
+      </section>
+    </div>
   );
 }
 
