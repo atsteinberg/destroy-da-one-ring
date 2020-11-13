9131b817bf1a97aff62b625a121d2612b5c25bb4
atsteinberg <alexts404dev@gmail.com>
2020-11-13 15:52:57 +0100

    feat: implement basic api call function

--- client/src/apiService.js
+++ client/src/apiService.js
@@ -1,7 +1,34 @@
-const postSolution = (solution) => {
-  const result = new Promise((res) => {
-    setTimeout(() => res(solution), 1000);
-  });
-  return result;
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.SERVER_PORT || 3001;
+const ADDRESS = process.env.SERVER_ADDRESS || 'https://localhost';
+const PATH = process.env.POST_PATH || 'game';
+
+const apiURL = `${ADDRESS}:${PORT}/${PATH}`;
+
+const handleError = (err) => {
+  console.error(err);
 };
+
+const postSolution = async (solution) => {
+  const options = {
+    header: {
+      method: 'POST',
+      contentType: 'application/json',
+      body: {
+        payload: solution,
+      },
+    },
+  };
+  let result;
+  try {
+    result = await fetch(apiURL, options);
+  } catch (err) {
+    handleError(err);
+  }
+  return result ? result.payload : undefined;
+};
+
 export default postSolution;
