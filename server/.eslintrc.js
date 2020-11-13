d73aa05e7db6742ce4891231e811a6465b66a9ff
atsteinberg <alexts404dev@gmail.com>
2020-11-13 19:58:53 +0100

    feat: add tests for game logic

---
+++ server/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    mocha: true,
+  },
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {},
+};
