ac59e59f5e2a7e74c6ffeb4423381a58671b1a98
atsteinberg <alexts404dev@gmail.com>
2020-11-14 18:55:17 +0100

    feat: add propTypes for classes

--- client/src/classes/index.js
+++ client/src/classes/index.js
@@ -1,9 +1,12 @@
-const Coordinate = require('./Coordinate');
-const TimeSlice = require('./TimeSlice');
-const TravelPath = require('./TravelPath');
+const { Coordinate, CoordinatePropType } = require('./Coordinate');
+const { TimeSlice, TimeSlicePropType } = require('./TimeSlice');
+const { TravelPath, TravelPathPropType } = require('./TravelPath');
 
 module.exports = {
   Coordinate,
+  CoordinatePropType,
   TimeSlice,
+  TimeSlicePropType,
   TravelPath,
+  TravelPathPropType,
 };
