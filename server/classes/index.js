8ca3ff93266e041eed9adbecee734ac80deac753
atsteinberg <alexts404dev@gmail.com>
2020-11-14 14:39:48 +0100

    feat: add TravelPath class

--- server/classes/index.js
+++ server/classes/index.js
@@ -1,7 +1,9 @@
 const Coordinate = require('./Coordinate');
 const TimeSlice = require('./TimeSlice');
+const TravelPath = require('./TravelPath');
 
 module.exports = {
   Coordinate,
   TimeSlice,
+  TravelPath,
 };
